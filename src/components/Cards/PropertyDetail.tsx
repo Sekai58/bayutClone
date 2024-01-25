80e9632f775a91e4571af34091846889529782c8
Sekai58 <amritachi58@gmail.com>
2024-01-25 19:03:56 +0545

    property details updated

--- src/components/Cards/PropertyDetail.tsx
+++ src/components/Cards/PropertyDetail.tsx
@@ -1,9 +1,7 @@
-import { FaPhoneAlt } from "react-icons/fa";
-import { FaWhatsapp } from "react-icons/fa";
-import { MdEmail } from "react-icons/md";
-import { IoBedOutline } from "react-icons/io5";
+import { FaRegHeart } from "react-icons/fa";
+import { IoBedOutline, IoShareOutline } from "react-icons/io5";
 import { GiBathtub } from "react-icons/gi";
-import { IoLocationOutline } from "react-icons/io5";
+import { PiGridFourLight } from "react-icons/pi";
 
 interface IProperty {
   price: number;
@@ -17,45 +15,49 @@ interface IProperty {
 
 const PropertyDetail: React.FC<IProperty> = (props: IProperty) => {
   return (
-    <div className="p-4 flex flex-col gap-2 w-full">
-      <h5 className=" text-xl font-medium block ">
-        AED <span className="text-2xl">{props.price}</span> Yearly
-      </h5>
-      <div>
-        <p className="text-md pb-4 flex items-center gap-3 font-medium flex-wrap">
-          {props.type} | <IoBedOutline />
-          {props.bed} | <GiBathtub /> {props.bath}| Area:
-          <span className="font-normal">{props.area}</span>
-        </p>
-        <p className="text-md pb-4 text-[#006169]">{props.description}</p>
-        <p className="text-md pb-4 flex items-center gap-1">
-          <IoLocationOutline />
-          {props.location}
-        </p>
-        <p className="text-[#2d3e9b] py-[1px] bg-[#ebeefc] rounded-md text-sm px-1 w-fit">
-          <span className="font-bold mr-2">+</span>Agent last visited on 16 of
-          January
-        </p>
-      </div>
-
-      <div className="flex justify-between items-center">
-        <div className="flex gap-2">
-          <button className="flex w-[4.5rem] py-1 items-center justify-center bg-[#e5eff0] text-[#006169] rounded-md font-semibold h-fit">
+    <div className="flex flex-col gap-2 w-full py-2">
+      <div className="flex w-full justify-between">
+        <h5 className=" text-2xl font-medium block ">
+          AED <span className="text-3xl font-semibold">{props.price}</span>{" "}
+          Yearly
+        </h5>
+        <div className="flex gap-2 items-center">
+          <button className="flex w-[4.5rem] p-1 items-center justify-center bg-[#e5eff0] text-[#006169] rounded-md font-semibold h-fit">
             <span className="mr-1">
-              <FaPhoneAlt />
+              <FaRegHeart />
             </span>
-            Call
+            Save
           </button>
-          <button className="flex w-[4.5rem] py-1 items-center justify-center bg-[#e5eff0] text-[#006169] rounded-md font-semibold h-fit">
+          <button className="flex w-[4.5rem] p-1 items-center justify-center bg-[#e5eff0] text-[#006169] rounded-md font-semibold h-fit">
             <span className="mr-1">
-              <MdEmail />
+              <IoShareOutline />
             </span>
-            Email
-          </button>
-          <button className="w-[4.5rem] py-2 text-md flex items-center justify-center bg-[#e9f7f0] text-[#249f62] rounded-md font-semibold h-fit">
-            <FaWhatsapp />
+            Share
           </button>
         </div>
+      </div>
+
+      <p className="text-lg font-medium pb-2">{props.location}</p>
+      <div>
+        <p className="text-lg pb-4 flex items-center gap-4 flex-wrap">
+          <span className="flex items-center gap-1">
+            <IoBedOutline />
+            {props.bed} Beds
+          </span>
+          <span className="flex items-center gap-1">
+            <GiBathtub /> {props.bath}Baths
+          </span>
+          <span className="flex items-center gap-1">
+            <PiGridFourLight />
+            {props.area} sqft
+          </span>
+        </p>
+        <p className="text-xl font-medium pb-4">{props.description}</p>
+
+        <p className="">
+          AX CAPITAL Real Estate is offering this beautiful Beautiful located in
+          Dubai Land in Cherrywood Community.
+        </p>
       </div>
     </div>
   );
